00b6b6d5f1ad67b3cc0ad8852e1dffca1af09f44
David Tolpin <david.tolpin@gmail.com>
2015-05-25 23:24:40 +0100

    scala interface to m!

--- src/main/scala/embang/scala.scala
+++ src/main/scala/embang/scala.scala
@@ -2,16 +2,25 @@ package embang.scala {
   import clojure.java.api._
   import clojure.core._
   import scala.collection.JavaConverters._ 
+  import scala.util.parsing.json._
 
   object sample {
+    // Setup Clojure runtime and require facility.
     private val require = Clojure.`var`("clojure.core", "require")
+    private val apply = Clojure.`var`("clojure.core", "apply")
+
+    // Load doquery function.
     private val doquery = { 
       require.invoke(Clojure.read("embang.trap"))
       require.invoke(Clojure.read("embang.json"))
       Clojure.`var`("embang.json", "doquery")
     }
-    def from (algorithm: String, query: String, value: String) = {
 
+    def from (algorithm: String = ":lmh",
+              query: String, value: String = "nil",
+              options: String = "") = {
+
+      // Parse the query namespace and name, and load the query.
       val (namespace, name) = 
         query split "/" match {
           case Array(namespace, name) => (namespace, name)
@@ -19,13 +28,17 @@ package embang.scala {
         }
       require.invoke(Clojure.read(namespace))
 
-      val samples = doquery.invoke(
+      // Fetch a lazy sequence of samples ...
+      val samples = apply.invoke(doquery,
         Clojure.read(algorithm), 
         Clojure.`var`(Clojure.read(namespace), Clojure.read(name)),
-        Clojure.read(value)
+        Clojure.read(value),
+        Clojure.read(options)
       ).asInstanceOf[java.util.Iterator[_]]
 
-      samples.asScala
+      // ... parsing JSON strings into JSON structures.
+      for(sample <- samples.asScala)
+        yield JSON.parseFull(sample.asInstanceOf[String])
     }
 
   }
