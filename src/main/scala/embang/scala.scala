d37d801a9c9dc4375b3c8b817fffc3363dfefcaf
David Tolpin <david.tolpin@gmail.com>
2015-05-29 11:41:13 +0100

    equalizer

--- src/main/scala/embang/scala.scala
+++ src/main/scala/embang/scala.scala
@@ -1,22 +1,27 @@
-package embang.scala {
-  import clojure.java.api._
-  import clojure.core._
-  import scala.collection.JavaConverters._ 
-  import scala.util.parsing.json._
-  import scala.collection.immutable.ListMap
+package embang
 
-// Scala interface to probabilistic programming language m!
-// Synopsis:
-//
-//    import embang.scala._
-//
-//    val X = draw from (query = "HMM")
-//    for (i <- 1 to 100) {
-//      val x = X.next
-//      println(x.predicts, x.logWeight)
-//    }
-//
-// predicts is a ListMap from predict identifiers to predict values.
+/**
+ * Scala interface to probabilistic programming language m!
+ * Synopsis:
+ *
+ *    import embang.scala._
+ *
+ *    val X = draw from (query = "HMM")
+ *    for (i <- 1 to 100) {
+ *      val x = X.next
+ *      println(x.predicts, x.logWeight)
+ *    }
+ *
+ * predicts is a ListMap from predict identifiers to predict values.
+ */
+
+package object scala {
+
+  import clojure.java.api.Clojure
+  import _root_.scala.collection.JavaConverters._ 
+  import _root_.scala.util.parsing.json.JSON
+  import _root_.scala.collection.immutable.ListMap
+  import util.Random
 
   class Sample(val predicts: Map[String, _], val logWeight: Double)
 
@@ -35,37 +40,53 @@ package embang.scala {
       sample match {
         case Some(s: Map[String, Any]) => 
           new Sample(ListMap()
-                       ++ s("predicts").asInstanceOf[List[List[Any]]]
-                            .map(l => (l(0).toString, l(1))),
-                     s("log-weight").asInstanceOf[Double])
-        case _ => new Sample(ListMap(), -1./0.)
+            ++ s("predicts").asInstanceOf[List[List[Any]]]
+              .map(l => (l(0).toString, l(1))),
+              s("log-weight").asInstanceOf[Double])
+          case _ => new Sample(ListMap(), -1./0.)
       }
     }
 
+    /** returns a squence of samples from query */
     def from (algorithm: String = ":lmh",
-              query: String, value: String = "nil",
-              options: String = "") = {
+      query: String, value: String = "nil",
+      options: String = "") = {
 
-      // Parse the query namespace and name, and load the query.
-      val (namespace, name) = 
-        query split "/" match {
-          case Array(namespace, name) => (namespace, name)
-          case _ => (query, query)
-        }
-      require.invoke(Clojure.read(namespace))
+        // Parse the query namespace and name, and load the query.
+        val (namespace, name) = 
+          query split "/" match {
+            case Array(namespace, name) => (namespace, name)
+            case _ => (query, query)
+          }
+        require.invoke(Clojure.read(namespace))
 
-      // Fetch a lazy sequence of samples ...
-      val samples = apply.invoke(doquery,
-        Clojure.read(algorithm), 
-        Clojure.`var`(Clojure.read(namespace), Clojure.read(name)),
-        Clojure.read(value),
-        Clojure.read("["+options+"]")
-      ).asInstanceOf[java.util.Iterator[_]]
+        // Fetch a lazy sequence of samples ...
+        val samples = apply.invoke(doquery,
+          Clojure.read(algorithm), 
+          Clojure.`var`(Clojure.read(namespace), Clojure.read(name)),
+          Clojure.read(value),
+          Clojure.read("["+options+"]")
+          ).asInstanceOf[java.util.Iterator[_]]
 
-      // ... parsing JSON strings into JSON structures.
-      for(sample <- samples.asScala)
-        yield asSample(JSON.parseFull(sample.asInstanceOf[String]))
+        // ... parsing JSON strings into JSON structures.
+        for(sample <- samples.asScala)
+          yield asSample(JSON.parseFull(sample.asInstanceOf[String]))
     }
+  }
 
+  /** equalizes samples so that they have the same weight */
+  def equalize(samples: Iterator[Sample]): Iterator[Sample] = {
+    ;; Using Metropolis-Hastings,
+    ;; thanks to Brooks Paige for the idea
+    var sample = samples.next
+    var equalized = new Sample(sample.predicts, 0.)
+    for(s <- samples) yield {
+      if(s.logWeight - sample.logWeight
+        >= math.log(Random.nextDouble)) {
+          sample = s
+          equalized = new Sample(s.predicts, 0.)
+        }
+        equalized
+    }
   }
 }
